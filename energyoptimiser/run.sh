9569065dd7a69d5347acb85ffc6b280b75c18c12
root <root@GeminiCLI.ivoozz.nl>
2026-03-11 17:02:27 +0100

    Fixed Supervisor 'Unknown error' by moving descriptions to translations and stabilizing Docker build

--- energyoptimiser/run.sh
+++ energyoptimiser/run.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
-# Get Supervisor Token
-export SUPERVISOR_TOKEN=$SUPERVISOR_TOKEN
-
 echo "Starting EnergyOptimiser Add-on..."
 
 # Start the Python Backend (FastAPI)
-# It will also serve the static UI files from /ui/dist
 cd /app
 python3 main.py
